c8787100fc434f2459556d69370957c84af60237
Blaster1111 <rudrashukla0711@gmail.com>
2025-02-10 11:24:48 +0530

    / path added to check availability of server

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactRoutes);
 
+app.get("/", (req, res) => {
+  res.send("BiteSpeed Intern Task Running");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
